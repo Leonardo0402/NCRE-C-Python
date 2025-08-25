4a10e6faae932404d1c8641a530eab5ed6ba16eb
Leonardo <2038511846@qq.com>
2025-08-26 00:02:06 +0800

    feat: 3rd homework

---
+++ II-C/code/XH/HomeworkCode/3.1.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//
+//int main() {
+//	printf("please enter a year: ");
+//	int year;
+//	if(scanf("%d", &year) != 1) return 0;
+//	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//		printf("%d is a leap year", year);
+//	}
+//	else {
+//		printf("%d is not a leap year", year);
+//	}
+//	return 0;
+//}
---
+++ II-C/code/XH/HomeworkCode/3.2.c
@@ -0,0 +1,14 @@
+//#include<stdio.h>
+//
+//int main() {
+//	int a, b;
+//	printf("please enter two nums: ");
+//	scanf("%d%d", &a, &b);
+//	if (a > b) {
+//		printf("%d", a - b);
+//	}
+//	else {
+//		printf("%d", b - a);
+//	}
+//	return 0;
+//}
